536655f70fc0e0f6dd437ec44d827224b407f391
kchen27 <kvc5823@psu.edu>
2020-09-10 20:46:01 +0000

    changed getGradePoint

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #Author: Kyle Chen kvc5823@psu.edu
 
-def GetGradePoint(grade):
+def getGradePoint(grade):
   if grade == "A":
     gp = 4.0
   elif grade == "A-":
@@ -24,16 +24,16 @@ def GetGradePoint(grade):
 def run():
   gp1 = str(input("Enter your course 1 letter grade:"))
   c1 = float(input("Enter your course 1 credit: "))
-  gp1 = GetGradePoint(gp1)
+  gp1 = getGradePoint(gp1)
   print(f"Grade point for course 1 is: {gp1}")
-  
+
   gp2 = str(input("Enter your course 2 letter grade:"))
   c2 = float(input("Enter your course 2 credit: "))
-  gp2 = GetGradePoint(gp2)
+  gp2 = getGradePoint(gp2)
   print(f"Grade point for course 2 is: {gp2}")
   gp3 = str(input("Enter your course 3 letter grade:"))
   c3 = float(input("Enter your course 3 credit: "))
-  gp3 = GetGradePoint(gp3)
+  gp3 = getGradePoint(gp3)
   print(f"Grade point for course 3 is: {gp3}")
   GPA = (gp1 * c1 + gp2 * c2 + gp3 * c3)/(c1 + c2 + c3)
   print(f"Your GPA is: {GPA}")
